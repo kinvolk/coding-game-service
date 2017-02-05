78d55a8b68949f8537970af3183f4284ff2a5672
Sam Spilsbury <sam@endlessm.com>
2017-02-06 04:04:35 +0800

    shim: Implement coding_game_service_app_integration_controller_new

--- lib/shim.c
+++ lib/shim.c
@@ -216,6 +216,11 @@ coding_game_service_app_integration_controller_init(CodingGameServiceAppIntegrat
   }
 }
 
+CodingGameServiceAppIntegrationController *
+coding_game_service_app_integration_controller_new () {
+  return g_object_new(CODING_GAME_SERVICE_TYPE_APP_INTEGRATION_CONTROLLER);
+}
+
 static void
 coding_game_service_app_integration_controller_class_init(CodingGameServiceAppIntegrationControllerClass *klass)
 {
